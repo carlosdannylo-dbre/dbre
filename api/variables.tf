7e24b6f1465946c6c75f0e1559ff8f977a8ffcc2
Carlos Dannylo <carlosdannylo@Carloss-MacBook-Pro.local>
2025-11-20 13:48:15 -0300

    ajuste

--- api/variables.tf
+++ api/variables.tf
@@ -1,3 +1,12 @@
-variable "org_id" {}
-variable "description" {}
-variable "org_roles" {}
+variable "org_id" {
+    description = "ID da organização do MongoDB Atlas onde a API Key será criada"
+    type        = string    
+}
+variable "description" {
+    description = "Descrição da API Key que será criada"
+    type        = string
+}
+variable "org_roles" {
+    description = "Lista de roles de organização atribuídas à API Key" 
+    type = list(string)   
+}       
