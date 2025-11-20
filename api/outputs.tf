405915bee8251f4631277ee332045617dfd8704e
Carlos Dannylo <carlosdannylo@Carloss-MacBook-Pro.local>
2025-11-20 13:49:05 -0300

    ajuste

--- api/outputs.tf
+++ api/outputs.tf
@@ -2,3 +2,11 @@ output "public_key" {
   value     = mongodbatlas_api_key.this.public_key
   sensitive = true
 }
+output "private_key" {
+  value     = mongodbatlas_api_key.this.private_key
+  sensitive = true
+}
+output "api_key_id" {
+  value     = mongodbatlas_api_key.this.id
+  sensitive = true
+}
