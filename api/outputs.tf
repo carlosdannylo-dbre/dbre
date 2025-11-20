f786d5fa3be31a6879189e0e9637137d2cba01fa
Carlos Dannylo <carlosdannylo@Carloss-MacBook-Pro.local>
2025-11-20 13:53:50 -0300

    ajuste

--- api/outputs.tf
+++ api/outputs.tf
@@ -1,12 +1,16 @@
-output "public_key" {
-  value     = mongodbatlas_api_key.this.public_key
-  sensitive = true
-}
-output "private_key" {
-  value     = mongodbatlas_api_key.this.private_key
-  sensitive = true
-}
 output "api_key_id" {
-  value     = mongodbatlas_api_key.this.id
-  sensitive = true
+  description = "ID interno da API key"
+  value       = mongodbatlas_api_key.this.api_key_id
+}
+
+output "public_key" {
+  description = "Public key gerada pela API key"
+  value       = mongodbatlas_api_key.this.public_key
+  sensitive   = true
+}
+
+output "private_key" {
+  description = "Private key gerada pela API key"
+  value       = mongodbatlas_api_key.this.private_key
+  sensitive   = true
 }
